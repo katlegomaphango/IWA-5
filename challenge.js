dc2685323b24b378042395ec9860f3c8ca7c0f0e
Katlego Maphango <mphangokatlego08@gmail.com>
2023-04-03 13:50:45 +0200

    REfacted the last if statement

--- challenge.js
+++ challenge.js
@@ -4,19 +4,23 @@ const NONE_SELECTED = '0'
 
 
 customers = 1
-const loc = 'NK'
+const loc = 'NAM'
 currency = null
 shipping = null
+let shippingPice = null
 
 if (loc === "RSA") { 
     shipping = 400 
     currency = 'R'
+    shippingPice = currency+shipping
 } else if (loc === "NAM") {
     shipping = 600 
     currency = '$'
+    shippingPice = currency+shipping
 } else {
     shipping = 800 
     currency = '$'
+    shippingPice = currency+shipping
 }
 
 const shoes = 300 * 1
@@ -27,15 +31,25 @@ const pens = 5 * NONE_SELECTED
 
 //shipping = null
 currency = '$'
-//                      300   + 70        + 0    + 0
-const cost_w_shipping = shoes + batteries + pens + shirts
+//                      300   + 70        + 0    + 0    + 500
+let cost_w_shipping = shoes + batteries + pens + shirts + toys
 
-if ( (cost_w_shipping >= 1000 || cost_w_shipping >= 60) && (loc == "NAM" || loc == "RSA")) {
-	if (customers === 1) {
+if (currency === '$') {
+    if(cost_w_shipping >= 60 && (loc == "NAM" || loc == "RSA") && customers === 1) {
         shipping = 0
-	}
+    }
+} else {
+    if(cost_w_shipping >= 1000 && (loc == "NAM" || loc == "RSA") && customers === 1) {
+        shipping = 0
+    }
 }
 
-if ((shipping = 0) && (customers !== 1)) { console.log(FREE_WARNING) }
+// if ( (cost_w_shipping >= 1000 || cost_w_shipping >= 60) && (loc == "NAM" || loc == "RSA")) {
+// 	if (customers === 1) {
+//         shipping = 0
+// 	}
+// }
 
-loc === 'NK' ? console.log(BANNED_WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping)
+if ((shipping === 0) && (customers !== 1)) { console.log(FREE_WARNING) }
+
+loc === 'NK' ? console.log(BANNED_WARNING) : console.log('Price', currency, shoes + batteries + pens + shirts + toys + shipping)

4ca8ab9053c035fecd9dc1c6e902712cae3c26ba
Katlego Maphango <mphangokatlego08@gmail.com>
2023-04-05 09:39:29 +0200

    Added comments

--- challenge.js
+++ challenge.js
@@ -9,18 +9,15 @@ currency = null
 shipping = null
 let shippingPice = null
 
-if (loc === "RSA") { 
-    shipping = 400 
+if (loc === "RSA") {             //if (location = RSA) { shipping === 400 && currency === 'R' }
+    shipping = 400
     currency = 'R'
-    shippingPice = currency+shipping
-} else if (loc === "NAM") {
-    shipping = 600 
-    currency = '$'
-    shippingPice = currency+shipping
+} else if (loc === "NAM") {     //if location = NAM
+    shipping = 600              //shipping = 600 
+    currency = '$'              //else shipping = 800
 } else {
     shipping = 800 
     currency = '$'
-    shippingPice = currency+shipping
 }
 
 const shoes = 300 * 1
@@ -43,13 +40,14 @@ if (currency === '$') {
         shipping = 0
     }
 }
-
-// if ( (cost_w_shipping >= 1000 || cost_w_shipping >= 60) && (loc == "NAM" || loc == "RSA")) {
-// 	if (customers === 1) {
-//         shipping = 0
+// if (shoes + batteries + pens + shirts > 1000 &&  ) {
+// 	if (location = NAM && customers < 2) {
+// 			if (location = RSA)
+// 		    shipping = 0 || calcShipping
+// 		}
 // 	}
 // }
 
 if ((shipping === 0) && (customers !== 1)) { console.log(FREE_WARNING) }
 
-loc === 'NK' ? console.log(BANNED_WARNING) : console.log('Price', currency, shoes + batteries + pens + shirts + toys + shipping)
+loc === 'NK' ? console.log(BANNED_WARNING) : console.log('Price', currency, cost_w_shipping + shipping)
